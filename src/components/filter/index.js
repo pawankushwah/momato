236bff6d54716218f20e0c461a9541ae79242c8a
Pawan Kushwah <pawankushwahmail@gmail.com>
2024-01-11 16:35:03 +0530

    added the iconUrl options in the filter component

--- src/components/filter/index.js
+++ src/components/filter/index.js
@@ -5,18 +5,19 @@ export default function Filter({buttonsData}) {
         <div className={`${styles.filterButtonContainer} maxWidth`}>
             {
                 buttonsData.map((data, index) => {
-                    return <FilterButton key={index} text={data.text} icon={data.icon} />
+                    return <FilterButton key={index} data={data} />
                 })
             }
         </div>
     )
 }
 
-function FilterButton({ text, icon }) {
+function FilterButton({ data }) {
     return (
         <span className={styles.filterButton}>
-            {icon && <i className={`fi fi-${icon}`}></i> }
-            <span>{text}</span>
+            {data.icon && <i className={`fi fi-${data.icon}`}></i> }
+            {data.iconUrl && <img src={data.iconUrl} alt={data.text} style={{width: "15px"}} /> }
+            <span>{data.text}</span>
         </span>
     )
 }
